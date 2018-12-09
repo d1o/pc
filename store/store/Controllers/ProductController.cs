a08a8d8f71bcdd87292f410b1d63926175727094
d1o <lisu27@op.pl>
2018-12-09 18:24:32 +0100

    stronicowanie

--- store/store/Controllers/ProductController.cs
+++ store/store/Controllers/ProductController.cs
@@ -4,18 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using store.Models;
+using store.Models.ViewModels;
 
 namespace store.Controllers
 {
     public class ProductController : Controller
     {
 		private IProductRepository repository;
+		public int PageSize = 5;
 
         public ProductController(IProductRepository rep)
 		{
 			repository = rep;
 		}
 
-		public ViewResult List() => View(repository.Products);
+		public ViewResult List(int productPage = 1)
+		{
+			return View(new ProductsListViewModel
+			{
+				Products = repository.Products
+					.OrderBy(p => p.ProductID)
+					.Skip((productPage - 1) * PageSize)
+					.Take(PageSize),
+				PagingInfo = new PagingInfo
+				{
+					CurrentPage = productPage,
+					ItemsPerPage = PageSize,
+					TotalItems = repository.Products.Count()
+				}
+			});
+
+		}
     }
 }
